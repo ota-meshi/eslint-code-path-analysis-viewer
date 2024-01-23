9ef9b4de1ac13829604dcefdb1466a7ea545aae7
ota-meshi <otameshiyo23@gmail.com>
2024-01-23 23:34:00 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslint4b from "vite-plugin-eslint4b";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
 
 export default defineConfig(() => ({
   plugins: [vue(), eslint4b()],

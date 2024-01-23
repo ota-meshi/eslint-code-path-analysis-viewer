4406f968c6bb8b135acc4d3407cc799ff578a8dd
ota-meshi <otameshiyo23@gmail.com>
2024-01-23 23:39:55 +0900

    fix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ESLint Code Path Analysis Viewer
 
+<https://ota-meshi.github.io/eslint-code-path-analysis-viewer/>
+
 ## Overview
 
 This is a viewer for the [Code Path Analysis] data that ESLint generates.
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import eslint4b from "vite-plugin-eslint4b";
 
 export default defineConfig(() => ({
+  base: "/eslint-code-path-analysis-viewer/",
   plugins: [vue(), eslint4b()],
   resolve: {
     alias: {},
